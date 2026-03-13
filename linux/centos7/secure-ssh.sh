a05a1482c77924bd914cb4a627e88b90080e21cb
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-03-13 17:26:33 -0400

    Add header comments to secure-ssh.sh
    Added header comments to secure-ssh.sh for clarity.

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,1 +1,5 @@
-File
+# secure-ssh.sh
+# author: sophialevasseur
+# adds a public key from the local or curled from the remote repo
+# removes remote ability to ssh in
+echo "ALL OF YOUR CODE GOES HERE"
